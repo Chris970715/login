3b640fd9569023a67d6c64992460a9a606652b43
Gyubum Kim <kgb2872@gmail.com>
2024-12-28 20:34:14 -0500

    Pratice_2

--- src/main/java/hello/login/domain/login/LoginService.java
+++ src/main/java/hello/login/domain/login/LoginService.java
@@ -1,4 +1,37 @@
 package hello.login.domain.login;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public LoginService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Member login(String loginId, String password) {
+        /*
+        Optional<Member> findMemberOptional = memberRepository.findByLoginId(loginId);
+        Member member =  findMemberOptional.get();
+
+        if(member.getPassword().equals(password)) {
+            return member;
+        } else {
+            return null;
+        }*/
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+
+
 }
