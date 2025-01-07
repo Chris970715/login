2f7e0854ae5cf44379d041e708fba9efe8960230
Gyubum Kim <kgb2872@gmail.com>
2025-01-06 23:50:55 -0500

    Filter_Test_2

--- src/main/java/hello/login/web/filter/LogFilter.java
+++ src/main/java/hello/login/web/filter/LogFilter.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.UUID;
 
 @Slf4j
 public class LogFilter implements Filter {
@@ -15,11 +16,28 @@ public class LogFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
         log.info("LogFilter doFilter");
 
         // ServletRequest is the parent class of HttpServletRequest. ServletRequest does not contain as many features as HttpServlet...
         HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+
+        String requestURI = httpRequest.getRequestURI();
+
+        String uuid  = UUID.randomUUID().toString();
+
+        try {
+            log.info("Request [{}] [{}]", uuid, requestURI);
+
+            // Calling next filters
+            chain.doFilter(servletRequest, servletResponse);
+
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            log.info("Response [{}] [{}]", uuid, requestURI);
+        }
 
     }
 
